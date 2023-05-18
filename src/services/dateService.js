23d86644361b7e62bba6ec7e8310d9331821bfaa
DIEGO SILVA GRACILIANO <64751203+Diego-Sil@users.noreply.github.com>
2023-05-18 00:09:22 -0300

    update dateService 2

--- src/services/dateService.js
+++ src/services/dateService.js
@@ -2,9 +2,8 @@ const getCurrentDate = () => {
 
     const myDate = new Date(Date.now()).toLocaleString().split(',')[0];
      
-    return console.log(myDate.split('/')[2]+'/'+myDate.split('/')[1]+'/'+myDate.split('/')[0]); 
+    return console.log(myDate.split('/')[2]+'-'+myDate.split('/')[1]+'-'+myDate.split('/')[0]); 
 };
-
 getCurrentDate()
 module.exports = {
     getCurrentDate
