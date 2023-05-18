f5b5b91c2dd419786a655d3fd1d2c284bcc367db
DIEGO SILVA GRACILIANO <64751203+Diego-Sil@users.noreply.github.com>
2023-05-18 00:28:17 -0300

    update dateService 5

--- src/services/dateService.js
+++ src/services/dateService.js
@@ -4,7 +4,7 @@ const getCurrentDate = () => {
     const ano = dataAtual.getFullYear();
     const mes = String(dataAtual.getMonth() + 1).padStart(2, '0');
     const dia = String(dataAtual.getDate()).padStart(2, '0');
-    return console.log(`${ano}-${mes}-${dia}`);
+    return `${ano}-${mes}-${dia}`;
 }
 
 
